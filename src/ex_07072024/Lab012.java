2c1f099d94702ae9c6b3b0f90ab0704b43911a88
Pankaj Singhal <pankajkumar357@gmail.com>
2025-01-28 16:27:41 +0530

    All Files added

---
+++ src/ex_07072024/Lab012.java
@@ -0,0 +1,11 @@
+package ex_07072024;
+
+public class Lab012 {
+
+    public static void main(String[] a) {
+
+        System.out.println("pankaj");
+
+     //public static void main(String[] a)   ----It can be a . b or args or anything
+    }
+}
---
+++ src/ex_07072024/Lab013.java
@@ -0,0 +1,18 @@
+package ex_07072024;
+
+public class Lab013 {
+    // Can I run class without a main method ? no
+   // run option ? no run option comes without main method
+    public static void main(String[] args) {
+
+       // static public we can write it down
+        // Instead of void we cannot write any other thing JVM will not recognize
+    }
+
+
+    public static void main() {
+   // line 11 is not a JVM function it will not recognize it as its not the entry point
+
+    }
+    }
+
