ba6e81f9a579ac6e92b5ed2e2e4b88ee3cb261b4
Pankaj Singhal <pankajkumar357@gmail.com>
2025-02-04 14:35:19 +0530

    All Files added

---
+++ src/ex_13072024/Lab047.java
@@ -0,0 +1,18 @@
+package ex_13072024;
+
+import java.sql.SQLOutput;
+
+public class Lab047 {
+
+    public static void main(String[] args) {
+         // || - OR
+
+        System.out.println(true || true);
+        System.out.println(true || false);
+        System.out.println(false || true);
+        System.out.println(false || false);
+
+          // OR Gate says that 0 , 0  will be fasle , 0,1 or 1, 0 will be always true
+
+    }
+}
