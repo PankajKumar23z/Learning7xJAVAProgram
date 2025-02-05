b0932a92e60dd3daf1948ef86404214c09125bc6
Pankaj Singhal <pankajkumar357@gmail.com>
2025-02-05 10:23:15 +0530

    All Files added

---
+++ src/ex_14072024/Lab050.java
@@ -0,0 +1,29 @@
+package ex_14072024;
+
+public class Lab050 {
+
+    public static void main(String[] args) {
+
+        // Operators
+
+        int age = 65;
+        int lambo = -1;
+        // data_type variable_name(Identifier) = variable_value(Literals)
+        // Unary    = Only 1 operand , operator
+        // age - operand , value - unary plus (+65) , = operator
+        // lambo - operand , value - unary minus (165) , = operator
+
+
+        // Binary Operator - we have 2 or more operands
+        int a =10;
+        int b = 23;
+        int sum = a+b ;
+        // Binary - Arthe +,-,*,/....
+        // a,b -> Operand , + operator
+
+        // Ternary Operator a,b,c - operand - we ahve 3 or more operators
+
+
+
+    }
+}
