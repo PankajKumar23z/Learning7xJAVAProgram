e795b12ac389035afb6640d5aa449777e5bfe1d7
Pankaj Singhal <pankajkumar357@gmail.com>
2025-02-05 10:51:25 +0530

    All Files added

--- src/ex_14072024/Lab060.java
+++ src/ex_14072024/Lab060.java
@@ -16,9 +16,23 @@ public class Lab060 {
         System.out.println(b);
 
 
-        int a_post = 10;
-        System.out.println(++a);
+        int a1 = 10;
+        System.out.println(++a1);
         // Exp = 11, a = 11
+
+
+        // POST
+        // value is stored in the result and incremented later
+
+        int a_post = 10;
+        System.out.println(a_post++);
+        System.out.println(a_post);
+        // Exp = 10, a = 11
+
+
+
+
+
 
 
     }
---
+++ src/ex_14072024/Lab061.java
@@ -0,0 +1,22 @@
+package ex_14072024;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Lab061 {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int result = a++;
+        System.out.println(a);
+
+        // Exp =10,   a=11
+
+
+        int a1 = 10;
+        int result1 = ++a1;
+        System.out.println(a1);
+
+
+        // Exp =11,   a=11
+    }
+}
